0265ddc84748d602c6dbb6129dff4ef855a67d5a
Irvin Martinez <irvin0329@outlook.com>
2022-05-31 19:03:47 -0600

    added git ignore file

--- express.js
+++ express.js
@@ -1,13 +1,18 @@
+const express = require("express")
 const server = express();
 
 server.get("/", (request,response)=>{
     response.send('You are in the home page, welcome!')
 } )
 
-server.post("/", (request,response)=>{
+server.post("/users", (request,response)=>{
     const respuesta = {
         hola: 'vato loco'
     }
+
+    //va a cnvertir el objeto a json
+    //lo va a mandar como respuesta
+    //y va a crear el header
     response.json(respuesta)
 } )
 
